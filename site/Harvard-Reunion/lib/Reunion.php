fab14c86b3625140c824759d8cb4a131e9e63cc9
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2011-02-23 17:25:57 -0500

    Moved Reunion config into a class.  Consolidated template variables.

--- site/Harvard-Reunion/lib/Reunion.php
+++ site/Harvard-Reunion/lib/Reunion.php
@@ -1,21 +1,88 @@
 <?php
 
 class Reunion {
-  $eventConfigs = array();
+  private $timezone = null;
+  private $start = null;
+  private $end = null;
+  private $eventConfigs = array();
+  private $attendee = null;
 
   function __construct() {
+    $this->timezone = new DateTimeZone(
+      $GLOBALS['siteConfig']->getVar('LOCAL_TIMEZONE', Config::LOG_ERRORS | Config::EXPAND_VALUE));
+  
     $configFile = realpath_exists(SITE_CONFIG_DIR.'/feeds/schedule.ini');
     if ($configFile) {
       $this->eventConfigs = parse_ini_file($configFile, true);
     }
     
+    $this->attendee = new Attendee();
   }
   
-  public function getAllFeeds() {
-    return $this->eventConfigs;
+  private function getConfigValue($key, $default=null) {
+    $event = $this->attendee->getGraduationClass();
+    if (isset($this->eventConfigs[$event], $this->eventConfigs[$event][$key])) {
+      return $this->eventConfigs[$event][$key];
+    }
+    
+    return $default;
   }
   
-  public function getFeed($event) {
-    return isset($this->eventConfigs[$event]) ? $this->eventConfigs[$event] : false;
+  public function getAttendee() {
+    return $this->attendee;
+  }
+  
+  public function getNumber() {
+    return $this->getConfigValue('REUNION_NUMBER', '0');
+  }
+  
+  public function getFacebookGroup() {
+    return $this->getConfigValue('FACEBOOK_GROUP', '0');
+  }
+  
+  public function getTwitterHashTag() {
+    return $this->getConfigValue('TWITTER_HASHTAG', '0');
+  }
+  
+  private function getDateTimeForDate($date) {
+    return new DateTime($date.' 00:00:00', $this->timezone);
+  }
+  public function getStartDate() {
+    if (!$this->start) {
+      $this->start = $this->getDateTimeForDate($this->getConfigValue('START_DATE', ''));
+    }
+    return $this->start;
+  }
+  public function getEndDate() {
+    if (!$this->end) {
+      $this->end = $this->getDateTimeForDate($this->getConfigValue('END_DATE', ''));
+    }
+    return $this->end;
+  }
+  
+  public function getDateDescription() {
+    $start = $this->getStartDate();
+    $end = $this->getEndDate();
+    
+    $startMonth = $start->format('M');
+    $startDay   = $start->format('j');
+    $endMonth   = $end->format('M');
+    $endDay     = $end->format('j');
+    
+    if ($startMonth == $endMonth) {
+      return "$startMonth $startDay-$endDay";
+    } else {
+      return "$startMonth $startDay-$endMonth $endDay";
+    }
+  }
+}
+
+class Attendee {  
+  public function getGraduationClass() {
+    return 2001;
+  }
+  
+  public function getFullName() {
+    return 'John Smith';
   }
 }
--- site/Harvard-Reunion/modules/home/SiteHomeModule.php
+++ site/Harvard-Reunion/modules/home/SiteHomeModule.php
@@ -20,41 +20,48 @@ class SiteHomeModule extends HomeModule {
     return $data;
   }
 
+  protected function initialize() {
+    $this->reunion = new Reunion();
+  }
 
   protected function initializeForPage() {
-    
+    $user = $this->reunion->getAttendee();    
   
     switch ($this->page) {
       case 'index':
         // TODO: get from backend
-        $attendee = 'John Smith';
+        $userInfo = array(
+          'fullname' => $user->getFullName(),
+          'class' => $user->getGraduationClass(),
+        );
 
         $reunionInfo = array(
-          'year'  => '10',
-          'dates' => 'May 27-29',
-          'class' => '2001',
-        );      
-        
-        $facebookGroup = array(
-          'name' => 'Harvard-Radcliff \'01',
-          'url'  => '',
-        );
-        $twitterTag = array(
-          'name' => '#hr10th',
-          'url'  => '',
+          'year'  => $this->reunion->getNumber(),
+          'dates' => $this->reunion->getDateDescription(),
         );
         
-        $recentPost = array(
+        $socialInfo = array(
+          'facebook' => array(
+            'name' => $this->reunion->getFacebookGroup(),
+            'url'  => '',
+          ),
+          'twitter' => array(
+            'name' => $this->reunion->getTwitterHashTag(),
+            'url'  => '',
+          ),
+          'recent' => null,
+        );
+        
+        $socialInfo['recent'] = array(
+          'type'    => 'twitter',
           'message' => 'big group going 2 John Harvard\'s in the Garage, everyone welcome',
           'author'  => 'Katarina Ragulin',
           'age'     => '12 mins',
         );
         
-        $this->assign('reunionInfo',   $reunionInfo);
-        $this->assign('attendee',      $attendee);
-        $this->assign('facebookGroup', $facebookGroup);
-        $this->assign('twitterTag',    $twitterTag);
-        $this->assign('recentPost',    $recentPost);
+        $this->assign('userInfo',    $userInfo);
+        $this->assign('reunionInfo', $reunionInfo);
+        $this->assign('socialInfo',  $socialInfo);
         break;
         
      case 'search':
