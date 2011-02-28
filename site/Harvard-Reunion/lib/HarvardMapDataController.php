b79cecc6eb7ad5f8dcdf02b5048aa584f3031c54
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2011-02-28 14:05:03 -0500

    Preliminary support for google static maps in Harvard

---
+++ site/Harvard-Reunion/lib/HarvardMapDataController.php
@@ -0,0 +1,131 @@
+<?php
+
+class HarvardMapDataController extends ArcGISDataController
+{
+    protected $returnsGeometry = true;
+    protected $isSearchLayer = false;
+
+    protected function init($args)
+    {
+        parent::init($args);
+
+        if (isset($args['DYNAMIC_ZOOM_LEVEL']))
+            $this->dynamicZoomLevel = $args['DYNAMIC_ZOOM_LEVEL'];
+        
+        if (isset($args['ARCGIS_LAYER_ID']))
+            $this->parser->setDefaultLayer($args['ARCGIS_LAYER_ID']);
+
+        if (isset($args['RETURNS_GEOMETRY']))
+            $this->returnsGeometry = $args['RETURNS_GEOMETRY'];
+    }
+    
+    public function getItem($name)
+    {
+        $theItem = null;
+        
+        if ($this->searchable) {
+            $this->initializeParser();
+            $this->initializeLayers();
+            $featureInfo = self::getBldgDataByNumber($name);
+            if ($featureInfo) {
+                $theItem = $this->parser->featureFromJSON($featureInfo);
+                // cheating here as i'm not sure when fields get
+                // returned as column ids or aliases
+                $theItem->setTitleField("Building Name");
+            }
+
+        } else {
+            $items = $this->items();
+            if (isset($items[$name])) {
+                $theItem = $items[$name];
+                if (!$this->returnsGeometry || $theItem->getGeometry() == null) {
+                    $featureInfo = $this->queryFeatureServer($theItem);
+                    $theItem->setGeometryType($featureInfo['geometryType']);
+                    $theItem->readGeometry($featureInfo['geometry']);
+                }
+
+                if ($theItem->getField('Photo') === null) {
+                    if (!isset($featureInfo))
+                        $featureInfo = $this->queryFeatureServer($theItem);
+
+                    $photoFields = array('PHOTO_FILE', 'Photo', 'Photo File');
+                    foreach ($photoFields as $field) {
+                        if (isset($featureInfo['attributes'][$field])) {
+                            $theItem->setField('Photo', $featureInfo['attributes'][$field]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return $theItem;
+    }
+    
+    private static function getSupplementaryFeatureData($bldgId, $searchField, $queryBase) {
+        // TODO don't use a shared cache file if queryBase isn't the default
+        $featureCache = new DiskCache($GLOBALS['siteConfig']->getVar('ARCGIS_FEATURE_CACHE'), 86400*7, true);
+        if (!$featureCache->isFresh($bldgId)) {
+            $query = http_build_query(array(
+                'searchText'     => $bldgId,
+                'searchFields'   => $searchField,
+                'contains'       => 'false',
+                'sr'             => '',
+                'layers'         => 0,
+                'returnGeometry' => 'true',
+                'f'              => 'json',
+                ));
+            $json = file_get_contents($queryBase . '/find?' . $query);
+            $jsonObj = json_decode($json, true);
+        
+            if (isset($jsonObj['results']) && count($jsonObj['results'])) {
+                $result = $jsonObj['results'][0];
+                $featureCache->write($result, $bldgId);
+            } else {
+                error_log("could not find building $bldgId", 0);
+            }
+        }
+        $result = $featureCache->read($bldgId);
+        return $result;
+    }
+    
+    public function getFeatureByField($searchField, $value) {
+        if (!$this->returnsGeometry) {
+            $queryBase = $GLOBALS['siteConfig']->getVar('ARCGIS_FEATURE_SERVER');
+        } else {
+            $queryBase = $this->baseURL;
+        }
+        $this->initializeParser();
+        $this->initializeLayers();
+        $featureInfo = self::getSupplementaryFeatureData($value, $searchField, $queryBase);
+        $feature = $this->parser->featureFromJSON($featureInfo);
+        return $feature;
+    }
+
+    private function queryFeatureServer($feature) {
+        if (!$this->returnsGeometry) {
+            $queryBase = $GLOBALS['siteConfig']->getVar('ARCGIS_FEATURE_SERVER');
+        } else {
+            $queryBase = $this->baseURL;
+        }
+        
+        $searchFieldCandidates = array('Building Number', 'Building Name', 'Building');
+        foreach ($searchFieldCandidates as $field) {
+            $searchField = $field;
+            $bldgId = $feature->getField($field);
+            if ($bldgId) {
+                break;
+            }
+        }
+        return self::getSupplementaryFeatureData($bldgId, $searchField, $queryBase);
+    }
+    
+    public static function getBldgDataByNumber($bldgId) {
+        $queryBase = $GLOBALS['siteConfig']->getVar('ARCGIS_FEATURE_SERVER');
+        return self::getSupplementaryFeatureData($bldgId, 'Building Number', $queryBase);
+    }
+    
+    public function setIsSearchLayer($isSearchLayer) {
+        $this->isSearchLayer = $isSearchlayer;
+    }
+
+}
---
+++ site/Harvard-Reunion/lib/HarvardMapSearch.php
@@ -0,0 +1,105 @@
+<?php
+
+require_once realpath(LIB_DIR.'/Maps/ArcGISParser.php');
+
+class HarvardMapSearch extends MapSearch {
+
+    private $searchFeedData;
+    private $searchController;
+    private $controllerLayerID;
+    
+    public function setFeedData($feeds) {
+        foreach ($feeds as $id => $feed) {
+            if ($feed['TITLE'] == 'Search Results') {
+                $this->searchFeedData = $feed;
+                $this->controllerLayerID = $id;
+                break;
+            }
+        }
+        parent::setFeedData($feeds);
+    }
+
+    public function searchCampusMap($query) {
+
+        $results = array();
+        $bldgIds = array();
+
+        $params = array(
+            'str' => $query,
+            'fmt' => 'json',
+            );
+    
+        $url = $GLOBALS['siteConfig']->getVar('MAP_SEARCH_URL').'?'.http_build_query($params);
+        $rawContent = file_get_contents($url);
+        $content = json_decode($rawContent, true);
+    
+        foreach ($content['items'] as $result) {
+            if (strlen($result['bld_num']) && !in_array($result['bld_num'], $bldgIds))
+                $bldgIds[] = $result['bld_num'];
+        }
+
+        if ($bldgIds) {
+            foreach ($bldgIds as $bldgId) {
+                $featureInfo = HarvardMapDataController::getBldgDataByNumber($bldgId);
+                $feature = new ArcGISFeature($featureInfo['attributes'], $featureInfo['geometry']);
+                $feature->setTitleField('Building Name');
+                $results[] = $feature;
+            }
+        }
+        return $results;
+    }
+    
+    public function getURLArgsForSearchResult($aResult) {
+        return array(
+            'featureindex' => $aResult->getField('Building Number'),
+            'category' => $this->controllerLayerID,
+            );
+    }
+	
+    public function getTitleForSearchResult($aResult) {
+        // results in this class are ArcGISFeature objects
+        // instead of dictionaries containing MapFeature objects
+        return $aResult->getTitle();
+    }
+
+    public function getLayerForSearchResult($featureID=null) {
+        if ($this->searchController == null) {
+            $this->searchController = MapDataController::factory($this->searchFeedData);
+            $this->searchController->setDebugMode($GLOBALS['siteConfig']->getVar('DATA_DEBUG'));
+        }
+    	return $this->searchController;
+	}
+
+    // search for courses
+    public function searchCampusMapForCourseLoc($query) {
+
+        $results = array();
+        $bldgIds = array();
+
+        $params = array(
+            'str' => $query,
+            'loc' => 'course',
+            );
+
+        $url = $GLOBALS['siteConfig']->getVar('MAP_SEARCH_URL').'?'.http_build_query($params);
+        $rawContent = file_get_contents($url);
+        $content = json_decode($rawContent, true);
+
+        foreach ($content['items'] as $resultObj) {
+            if (!in_array($resultObj['bld_num'], $bldgIds))
+                $bldgIds[] = $resultObj['bld_num'];
+        }
+
+        if ($bldgIds) {
+            foreach ($bldgIds as $bldgId) {
+                $featureInfo = HarvardMapDataController::getBldgDataByNumber($bldgId);
+                $feature = new ArcGISFeature($featureInfo['attributes'], $featureInfo['geometry']);
+                $feature->setTitleField('Building Name');
+                $results[] = $feature;
+            }
+        }
+
+        return $results;
+    }
+
+}
