97c13bb3e6d311b6c2852a347c3be1d67df6db6f
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2011-03-26 22:29:39 -0400

    Tweaks to prevent facebook errors from generating warnings Tweak to try to get facebook logout to work better

--- site/Harvard-Reunion/lib/FacebookGroup.php
+++ site/Harvard-Reunion/lib/FacebookGroup.php
@@ -581,6 +581,8 @@ class FacebookGroup {
   // 
   
   private function graphQuery($type, $id, $method='GET', $params=array()) {
+    $results = array();
+  
     if (is_array($method) && empty($params)) {
       $params = $method;
       $method = 'GET';
@@ -624,10 +626,12 @@ class FacebookGroup {
       }
     }
     
-    return $results;
+    return $results ? $results : array();
   }
   
   private function fqlQuery($type, $query, $cacheSuffix='') {
+    $results = array();
+  
     $cache = $this->getCacheForQuery($type);
     $cacheName = $type.'_'.($cacheSuffix ? $cacheSuffix : md5($query));
     
@@ -654,7 +658,7 @@ class FacebookGroup {
       }
     }
     
-    return $results;
+    return $results ? $results : array();
   }  
 }
 
@@ -724,7 +728,10 @@ class ReunionFacebook extends Facebook {
     return $this->getUrl(
       'www',
       'logout.php',
-      array('next' => $redirectURL)
+      array(
+        'api_key' => $this->getAppId(),
+        'next'    => $redirectURL,
+      )
     );
   }
   
